16a98867b1ee5e11d88f8fc64f1f84e08bfa4543
Elvis Saavedra <elvis.saavedra.segura@gmail.com>
2024-09-06 00:07:59 -0500

    debound search

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,5 +1,5 @@
 'use client'
-
+import { useDebouncedCallback } from 'use-debounce';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
@@ -8,7 +8,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
+
     const params = new URLSearchParams(searchParams)
     if (term) {
       params.set('query', term)
@@ -17,7 +19,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
 
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 500);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
